4e8514c3934211c38b10895bac391f328fdfcc0c
Joey <107812238+JosephVasquez592@users.noreply.github.com>
2022-08-18 00:13:22 -0600

    finishing for the night

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,8 @@ var topDate = document.getElementsByClassName('lead');
 var currentTime = moment();
 $("#currentDay").text(currentTime.format("dddd, MMMM DD, YYYY LTS"));
 var hourBlocks = document.getElementsByClassName("container");
-var createUl = document.createElement('ul')
+var createUl = document.createElement('ul');
+var createLi = document.createElement('li');
 
 // Arrays
 var amPm = ["12 Midnight", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 Noon", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"];
@@ -16,12 +17,10 @@ function updateTime() {
  
 };
 
-function createBlockAm() {
-    var createLi = document.createElement('li')
-    
-    while (i <= 24) {
-        
-
+function createBlockAm() {    
+    for (var i = 0; i < amPm.length; i++) {
+        createLi.textContent = createBlockAm;
+        createUl.appendChild()
     }
 }
 
