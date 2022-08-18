764486ced3c24e765024a6337553fa1c3f806285
Joey <107812238+JosephVasquez592@users.noreply.github.com>
2022-08-17 23:03:07 -0600

    added js file and further organization

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// Global Variables
+var topDate = document.getElementById('currentDay');
+var currentTime = moment();
+$("#currentDay").text(currentTime.format("dddd, MMMM DD, YYYY LTS"));
+var hourBlocks = document.getElementsByClassName("container");
+
+// Functions
+function updateTime() {
+    for (var i = 0; i < currentTime; i++) {
+    
+}
+};
+
+
+
+// Calling Functions and Variables
+currentTime;
